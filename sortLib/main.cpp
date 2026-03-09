2f075bc17f8b2c6ff72122a68a81ae65253f617c
DoggieArc <doge.jack@mail.ru>
2026-03-10 00:28:36 +0700

    test file

---
+++ sortLib/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "sortLib.h"
+
+#define SIZE 18
+#define MAX_VALUE 10000
+#define MIN_VALUE 0
+
+using namespace std;
+
+template <typename Type>
+void fillWithRandom(Type* array, int size)
+{
+    srand(time(NULL));
+    for(int i = 0; i < size; ++i)
+        array[i] = (double)(rand() % (MAX_VALUE-MIN_VALUE) + MIN_VALUE+1)/100;
+}
+
+template <typename Type>
+void printArray(Type* array, int size)
+{
+    for(int i = 0; i < size; ++i)
+        cout << array[i] << ' ';
+    cout << endl;
+}
+
+int main()
+{
+    char method;
+    double* array = new double[SIZE];
+
+    fillWithRandom(array, SIZE);
+    cout << "The array: ";
+    printArray(array, SIZE);
+
+    cout << "1) Bubble sort" << endl
+    << "2) Shaker sort" << endl
+    << "Choose method of sort: ";
+    cin >> method;
+    switch(method){
+        case '1':
+            sortBubble(array, SIZE);
+            printArray(array, SIZE);
+            sortBubble(array, SIZE, M_DECEND);
+            printArray(array, SIZE);
+            break;
+        case '2':
+            sortShaker(array, SIZE);
+            printArray(array, SIZE);
+            sortShaker(array, SIZE, M_DECEND);
+            printArray(array, SIZE);
+            break;
+        default:
+            cout << "Error";
+    }
+
+    return 0;
+}
